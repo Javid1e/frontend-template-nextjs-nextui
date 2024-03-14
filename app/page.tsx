e2135f857bd4507ff65c854c5802b6f6f116b957
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-03-14 09:06:52 +0330

    Update page.tsx For Using Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { IranSans, IranYekan } from '@/config/fonts';
+import clsx from 'clsx';
 export default function Home() {
   return (
-    <p>
-      استفاده از کنابخانه NextUI در Next.js به همراه فونت ایران سنس و ایران یکان
-    </p>
+    <>
+      <p>استفاده از کنابخانه NextUI در Next.js</p>
+      <br />
+      <p className={clsx('font-iransans text-2xl', IranSans.variable)}>
+        به همراه فونت ایران سنس
+      </p>
+      <br />
+      <p className={clsx('font-iranyekan text-2xl', IranYekan.variable)}>
+        و فونت ایران یکان
+      </p>
+    </>
   );
 }
